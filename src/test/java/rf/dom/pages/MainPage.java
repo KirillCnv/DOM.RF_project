918445708c23ce8001c89b71a13168ac719b9afb
Kirill <kirillcnv@gmail.com>
2023-01-20 15:15:40 +0300

    update MainPage and MainTest

--- src/test/java/rf/dom/pages/MainPage.java
+++ src/test/java/rf/dom/pages/MainPage.java
@@ -8,7 +8,10 @@ import static com.codeborne.selenide.Selenide.*;
 public class MainPage {
 
     private SelenideElement
-            allDirections = $(".header-buttons-internal__text");
+            allDirections = $(".header-buttons-internal__text"),
+            searchIcon = $(".header-buttons__search"),
+            searchString = $(".search__field"),
+            burgerMenu = $(".burger-button");
 
     public MainPage openPage() {
         open("/");
@@ -17,12 +20,41 @@ public class MainPage {
 
     public MainPage openAllDirections() {
         allDirections.click();
-        return  this;
+        return this;
     }
 
-    public MainPage checkMenuDirection(String value){
+    public MainPage checkMenuDirection(String value) {
         $$(".internal-list").shouldHave(CollectionCondition.texts(value));
+        return this;
+    }
 
-        return  this;
+    public MainPage clickSearch() {
+        searchIcon.click();
+
+        return this;
+    }
+
+    public MainPage setValueSearchBar(String value) {
+        searchString.setValue(value).pressEnter();
+
+        return this;
+    }
+
+    public MainPage checkSearchResult(String value) {
+        $$(".search__result").shouldHave(CollectionCondition.texts(value));
+
+        return this;
+    }
+
+    public MainPage clickBurgerMenu() {
+        burgerMenu.click();
+
+        return this;
+    }
+
+    public MainPage checkElementsBurgerMenu(String value){
+        $$(".menu-burger-nav__main").shouldHave(CollectionCondition.texts(value));
+
+        return this;
     }
 }
--- src/test/java/rf/dom/tests/MainTest.java
+++ src/test/java/rf/dom/tests/MainTest.java
@@ -1,17 +1,46 @@
 package rf.dom.tests;
 
+import com.codeborne.selenide.CollectionCondition;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.$$;
+
 
 public class MainTest extends TestConfig {
-    @ParameterizedTest
-    @ValueSource(strings = {"ДОМ.РФ", "Аренда ДОМ.РФ","Наш ДОМ.РФ","ЩЛЗ","СОПФ агент","ДОМ.РФ Девелопмент","ДОМ.РФ Земля и Недвижимость",
-            "Городская среда","Программы господдержки","Цифровая академия","Ипотечный агент"})
+    @ParameterizedTest(name = "Проверка видимости элемента {0} в меню направлений")
+    @ValueSource(strings = {"ДОМ.РФ", "Аренда ДОМ.РФ", "Наш ДОМ.РФ", "ЩЛЗ", "СОПФ агент", "ДОМ.РФ Девелопмент", "ДОМ.РФ Земля и Недвижимость",
+            "Городская среда", "Программы господдержки", "Цифровая академия", "Ипотечный агент"})
     void directionsTest(String testDate) {
         mainPage.openPage()
                 .openAllDirections()
                 .checkMenuDirection(testDate);
+    }
 
+    @Test
+    @DisplayName("Проверка поисковой строки")
+    void searchTest() {
+        mainPage.openPage()
+                .clickSearch()
+                .setValueSearchBar("Ипотека")
+                .checkSearchResult("Ипотека для IT-специалистов")
+                .checkSearchResult("Льготная ипотека")
+                .checkSearchResult("Семейная ипотека");
+    }
+
+    @Test
+    @DisplayName("Проверка элементов Burger меню")
+    void burgerMenuTest(){
+        mainPage.openPage()
+                .clickBurgerMenu()
+                .checkElementsBurgerMenu("О компании")
+                .checkElementsBurgerMenu("Карьера")
+                .checkElementsBurgerMenu("Инвесторам")
+                .checkElementsBurgerMenu("Аналитика")
+                .checkElementsBurgerMenu("Новости")
+                .checkElementsBurgerMenu("Контакты");
     }
 }
